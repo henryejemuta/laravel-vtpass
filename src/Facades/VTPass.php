cba033b5da712bc5283f2bee2649f7e6c6dbf7f4
Henry Ejemuta <henryakin15@gmail.com>
2020-09-29 15:04:48 +0100

    Added Anotation to VTPass Facade

--- src/Facades/VTPass.php
+++ src/Facades/VTPass.php
@@ -9,8 +9,32 @@
 
 namespace HenryEjemuta\LaravelVTPass\Facades;
 
+use HenryEjemuta\LaravelVTPass\Classes\VTPassResponse;
 use Illuminate\Support\Facades\Facade;
 
+
+/**
+ * @method static VTPassResponse getServicesCategories()
+ * @method static VTPassResponse getServiceID($serviceCategoryIdentifier)
+ * @method static VTPassResponse getVariationCodes($serviceID)
+ * @method static VTPassResponse getProductOptions(string $serviceID, string $name)
+ * @method static VTPassResponse purchaseAirtime(string $requestId, string $serviceID, int $amount, $phoneNumber)
+ * @method static VTPassResponse queryTransactionStatus(string $requestId)
+ * @method static VTPassResponse purchaseProduct(string $requestId, string $serviceID, $billersCode, $variationCode, $phoneNumber, int $amount = 0)
+ * @method static VTPassResponse purchaseData(string $requestId, string $serviceID, $phoneNumber, $variationCode, $customerPhoneNumber, int $amount = 0)
+ * @method static VTPassResponse getSmileBundles()
+ * @method static VTPassResponse verifyMerchant($billersCode, $serviceID, $type = null)
+ * @method static VTPassResponse verifySmileCustomerByID($customerID)
+ * @method static VTPassResponse verifySmileCustomer($customerUniqueDetail, $detailType)
+ * @method static VTPassResponse verifySmileCustomerByEmail($customerEmail)
+ * @method static VTPassResponse verifySmileCustomerByPhone($customerPhone)
+ * @method static VTPassResponse buySmileData(string $requestId, $smilePhoneNumber, $variationCode, $phoneNumber, int $amount = 0)
+ * @method static VTPassResponse payGoTV(string $requestId, $smartCartNumber, $variationCode, $phoneNumber, int $amount = 0)
+ * @method static VTPassResponse verifyElectricityBillMeterNumber($customerMeterNumber, $serviceID, $type)
+ * @method static VTPassResponse buyElectricity(string $requestId, $serviceID, $customerMeterNumber, $type, $phoneNumber, int $amount) @throws VTPassErrorException
+ *
+ * @see \Henryejemuta\LaravelVTPass\VTPass
+ */
 class VTPass extends Facade
 {
     protected static function getFacadeAccessor()
